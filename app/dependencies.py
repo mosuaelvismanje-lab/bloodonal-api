c3c374533707ea80af1bbbd3f645fe4814b7a1ee
Elvis Manje Mosua <mosuaelvismanje@gmail.com>
2025-12-03 05:37:36 +0100

    Refactor get_db to use session dependency
    Refactor get_db function to accept an AsyncSession as a parameter.

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,11 +1,8 @@
+
 # app/dependencies.py
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
 from app.database import get_async_session
 
-async def get_db() -> AsyncSession:
-    """
-    Async dependency to provide an AsyncSession for FastAPI endpoints.
-    """
-    async with get_async_session() as session:
-        yield session
+async def get_db(session: AsyncSession = Depends(get_async_session)):
+    return session
